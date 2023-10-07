020ba7266b4c0d362158b3041447d510af513651
Mehul-Kumar-27 <mehul2002kumar@gmail.com>
2023-10-07 20:07:40 +0530

    Sign In and verification

--- src/controllers/user_controller.ts
+++ src/controllers/user_controller.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express";
 import { createUser, getAllUsers } from "../services/user_service";
 import logger from "../utils/logger";
-import { CreateUserInput } from "../schema/user-schema";
+
 
 export async function createUserHandeller(req: Request, res: Response) {
   const user = await createUser(req.body)
